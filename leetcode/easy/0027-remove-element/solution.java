5a2d44654a3a06148277e5eaaa2f9f9de45486ad
Kiran karthick.S <mrkiranvaku@gmail.com>
2026-08-02 09:58:54 +0530

    ✅ Remove Element | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 43.8 MB | 2026-08-02

---
+++ leetcode/easy/0027-remove-element/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int write=0;
+        for(int i=0;i<nums.length; i++){
+            if(nums[i]!=val){
+
+
+                 
+                 nums[write]=nums[i];
+                 write++;
+            }
+        }
+        return write;
+    }
+}
