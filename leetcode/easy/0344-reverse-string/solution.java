bc206e6b767f5d6040b991c9788dbcd6396646b8
KIRAN KARTHICK S <mrkiranvaku@gmail.com>
2026-09-12 10:22:45 +0530

    ✅ Reverse String | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 47.8 MB | 2026-09-12

--- leetcode/easy/0344-reverse-string/solution.java
+++ leetcode/easy/0344-reverse-string/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public void reverseString(char[] s) {
         int  first=0;
-        char temp;
         int last=(s.length)-1;
         while (first<last){
-            temp=s[first];
+            char temp=s[first];
             s[first]=s[last];
             s[last]=temp;
             first++;
