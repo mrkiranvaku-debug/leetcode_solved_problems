57514a573fc147a8f2f7718270a89eac5eb84583
KIRAN KARTHICK S <mrkiranvaku@gmail.com>
2026-09-13 08:37:12 +0530

    ✅ Q1. Cyclically Shift Rows and Columns | Easy | Java | Runtime: 1 ms (100.00%) | Memory: 47.6 MB | 2026-09-13

--- leetcode/easy/4416-cyclically-shift-rows-and-columns/solution.java
+++ leetcode/easy/4416-cyclically-shift-rows-and-columns/solution.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[][] cyclicShift(int n, int[][] grid, int[] rowShift, int[] colShift) {
-        int[][] temp = new int[n][n];
         int[][] res = new int[n][n];
 
-        // Step 1: Cyclically left shift each row
-        // Element at (i, j) moves to column (j - rowShift[i] + n) % n
         for (int i = 0; i < n; i++) {
-            int shift = rowShift[i];
+            // intermediate column after row shift
+            int midCol = (i - rowShift[i] % n + n) % n; 
+            
             for (int j = 0; j < n; j++) {
-                int newCol = (j - shift % n + n) % n;
-                temp[i][newCol] = grid[i][j];
-            }
-        }
+                // final column position after applying shift to midCol
+                int finalCol = (j - rowShift[i] % n + n) % n;
+                // final row position after applying shift to column finalCol
+                int finalRow = (i - colShift[finalCol] % n + n) % n;
 
-        // Step 2: Cyclically upward shift each column
-        // Element at (i, j) moves to row (i - colShift[j] + n) % n
-        for (int j = 0; j < n; j++) {
-            int shift = colShift[j];
-            for (int i = 0; i < n; i++) {
-                int newRow = (i - shift % n + n) % n;
-                res[newRow][j] = temp[i][j];
+                res[finalRow][finalCol] = grid[i][j];
             }
         }
 
